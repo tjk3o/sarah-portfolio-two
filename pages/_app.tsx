e490ec79a3c76ee95cf027a28ff74c1b6714809c
tjk3o <tom.keogh@made.com>
2022-10-05 13:10:33 +0100

    Attempt to remove flash of unstyled content with useEffect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.css';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.documentElement.style.visibility = 'visible';
+  }, []);
   return <Component {...pageProps} />;
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document(): JSX.Element {
   return (
-    <Html>
+    <Html lang='en' style={{ visibility: 'hidden' }}>
       <Head>
         <link rel='shortcut icon' href='/favicon.ico' />
         <link rel='apple-touch-icon' href='/touch-icon-iphone.png' />
